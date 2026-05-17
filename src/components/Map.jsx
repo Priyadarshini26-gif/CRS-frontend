e74185580f8129f52ec0e674df8457f58aa056bb
discount-backend <priyadarshinib416@gmail.com>
2026-05-17 10:47:06 +0530

    color commit

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -36,11 +36,11 @@ const createCategoryMarker = (color) => {
   });
 };
 
-// Create dark red marker icon for location (pin shape)
+// Create red marker icon for location (Google Maps style)
 const createLocationMarker = () => {
   return L.divIcon({
     html: `<svg width="32" height="41" viewBox="0 0 32 41" xmlns="http://www.w3.org/2000/svg">
-      <path d="M16 0C7.163 0 0 7.163 0 16c0 9 16 25 16 25s16-16 16-25c0-8.837-7.163-16-16-16z" fill="#8B0000"/>
+      <path d="M16 0C7.163 0 0 7.163 0 16c0 9 16 25 16 25s16-16 16-25c0-8.837-7.163-16-16-16z" fill="#FF0000"/>
       <circle cx="16" cy="16" r="6" fill="white"/>
     </svg>`,
     iconSize: [32, 41],
